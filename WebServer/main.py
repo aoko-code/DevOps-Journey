55c4681d759188535b136ed266cf8802f1956731
aoko-code <cynthiakhamenge@yahoo.com>
2024-06-11 15:26:28 +0300

    added comments explainingthe codes

--- WebServer/main.py
+++ WebServer/main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask #import flask class
 
-app = Flask(__name__)
+app = Flask(__name__) #create an instance
 
-@app.route('/')
-def kham_code():
-    return 'Hello there'
+@app.route('/') #decorator
+def kham_code(): #define a function
+    return 'Hello there' #return statement
 
 if __name__== '__main__':
-    app.run(debug=True)
+    app.run(debug=True) #enable debug mode
